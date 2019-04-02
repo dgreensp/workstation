9505211b0a602d3b6ed8c60d1cbdd5538fee8fec
David Greenspan <davidgreenspan@gmail.com>
2019-04-02 11:41:36 -0700

    good ax progress; indentation of single words

--- docs/bundle.js
+++ docs/bundle.js
@@ -2,6 +2,13 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     return new (P || (P = Promise))(function (resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
@@ -9,13 +16,6 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-};
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
-    result["default"] = mod;
-    return result;
 };
 /**
  * @license almond 0.3.3 Copyright jQuery Foundation and other contributors.
@@ -546,7 +546,7 @@ define("lib/live/index", ["require", "exports", "lib/live/Receiver", "lib/live/L
 define("demos/ax1/index", ["require", "exports", "react", "react-dom", "lib/live/index"], function (require, exports, react_3, react_dom_1, live_1) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    react_3 = __importDefault(react_3);
+    react_3 = __importStar(react_3);
     react_dom_1 = __importDefault(react_dom_1);
     function App() {
         const input = live_1.useLiveVar(() => '');
@@ -559,8 +559,84 @@ define("demos/ax1/index", ["require", "exports", "react", "react-dom", "lib/live
                             react_3.default.createElement("textarea", { className: "form-control text-monospace input", value: inputValue, onChange: e => input(e.target.value) })))),
                 react_3.default.createElement("div", { className: "col-sm colRight" },
                     react_3.default.createElement("div", { className: "px-3 pt-4" },
-                        react_3.default.createElement("pre", null,
-                            react_3.default.createElement("code", null, inputValue)))))));
+                        react_3.default.createElement(ParserOutput, { input: inputValue }))))));
+    }
+    function ParserOutput({ input }) {
+        const result = react_3.useMemo(() => parseAx(input), [input]);
+        return (react_3.default.createElement("pre", null,
+            react_3.default.createElement("code", { className: result.type }, JSON.stringify(result, null, 2))));
+    }
+    const NO_PARAMETERS = Object.seal([]); // shared object for performance
+    function pushParameter(command, parameter) {
+        if (!command.parameters) {
+            command.parameters = [];
+        }
+        command.parameters.push(parameter);
+    }
+    function completeAx(partial) {
+        return {
+            name: partial.name,
+            parameters: partial.parameters ? partial.parameters.map(completeAx) : NO_PARAMETERS,
+        };
+    }
+    class Scanner {
+        constructor(input) {
+            this.input = input;
+            this.index = 0;
+        }
+        match(re) {
+            if (!re.sticky) {
+                throw new Error('reg ex must be sticky');
+            }
+            re.lastIndex = this.index;
+            const match = re.exec(this.input);
+            if (!match) {
+                return undefined;
+            }
+            this.index = re.lastIndex;
+            return match;
+        }
+        matchAlways(re) {
+            return this.match(re) || [''];
+        }
+    }
+    function parseAx(input) {
+        const lines = input.split('\n');
+        let indentSpacesCount = 0;
+        const INDENT_SIZE = 2;
+        const root = { name: '' };
+        let currentCommand = root;
+        const commandStack = [root];
+        let lineNumber = 1;
+        function failure(message) {
+            return { type: 'failure', errors: [{ lineNumber, message }] };
+        }
+        for (const line of lines) {
+            const s = new Scanner(line);
+            const [spaces] = s.matchAlways(/ */y);
+            while (spaces.length <= indentSpacesCount - INDENT_SIZE) {
+                commandStack.pop();
+                currentCommand = commandStack[commandStack.length - 1];
+                indentSpacesCount -= INDENT_SIZE;
+            }
+            if (spaces.length !== indentSpacesCount) {
+                return failure('indent');
+            }
+            const wordMatch = s.match(/\S+/y);
+            if (!wordMatch) {
+                return failure('expected command');
+            }
+            const newCommand = { name: wordMatch[0] };
+            pushParameter(currentCommand, newCommand);
+            currentCommand = newCommand;
+            commandStack.push(newCommand);
+            indentSpacesCount += INDENT_SIZE;
+            if (!s.match(/$/my)) {
+                return failure('expected end of line');
+            }
+            lineNumber++;
+        }
+        return { type: 'success', result: completeAx(root) };
     }
     const root = document.getElementById('root');
     react_dom_1.default.render(react_3.default.createElement(App, null), root);
--- docs/demos/ax1/index.tsx
+++ docs/demos/ax1/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import ReactDOM from 'react-dom'
 import { useLiveVar, useListen } from 'lib/live'
 
@@ -22,14 +22,118 @@ function App() {
         </div>
         <div className="col-sm colRight">
           <div className="px-3 pt-4">
-            <pre>
-              <code>{inputValue}</code>
-            </pre>
+            <ParserOutput input={inputValue} />
           </div>
         </div>
       </div>
     </main>
   )
+}
+
+function ParserOutput({ input }: { input: string }) {
+  const result = useMemo(() => parseAx(input), [input])
+  return (
+    <pre>
+      <code className={result.type}>{JSON.stringify(result, null, 2)}</code>
+    </pre>
+  )
+}
+
+interface Ax {
+  readonly name: string
+  readonly parameters: ReadonlyArray<Ax>
+}
+
+interface PartialAx {
+  readonly name: string
+  parameters?: PartialAx[]
+}
+
+const NO_PARAMETERS = Object.seal([]) // shared object for performance
+
+function pushParameter(command: PartialAx, parameter: PartialAx): void {
+  if (!command.parameters) {
+    command.parameters = []
+  }
+  command.parameters.push(parameter)
+}
+
+function completeAx(partial: PartialAx): Ax {
+  return {
+    name: partial.name,
+    parameters: partial.parameters ? partial.parameters.map(completeAx) : NO_PARAMETERS,
+  }
+}
+
+class Scanner {
+  constructor(private input: string) {}
+  index = 0
+  match(re: RegExp): string[] | undefined {
+    if (!re.sticky) {
+      throw new Error('reg ex must be sticky')
+    }
+    re.lastIndex = this.index
+    const match = re.exec(this.input)
+    if (!match) {
+      return undefined
+    }
+    this.index = re.lastIndex
+    return match
+  }
+  matchAlways(re: RegExp): string[] {
+    return this.match(re) || ['']
+  }
+}
+
+interface ParseSuccess {
+  type: 'success'
+  result: Ax
+}
+
+interface ParseFailure {
+  type: 'failure'
+  errors: { lineNumber: number; message: string }[]
+}
+
+type ParseResult = ParseSuccess | ParseFailure
+
+function parseAx(input: string): ParseResult {
+  const lines = input.split('\n')
+  let indentSpacesCount = 0
+  const INDENT_SIZE = 2;
+  const root: PartialAx = { name: '' }
+  let currentCommand = root
+  const commandStack = [root]
+  let lineNumber = 1
+  function failure(message: string): ParseFailure {
+    return { type: 'failure', errors: [{ lineNumber, message }] }
+  }
+  for (const line of lines) {
+    const s = new Scanner(line)
+    const [spaces] = s.matchAlways(/ */y)
+    while (spaces.length <= indentSpacesCount - INDENT_SIZE) {
+      commandStack.pop()
+      currentCommand = commandStack[commandStack.length - 1]
+      indentSpacesCount -= INDENT_SIZE
+    }
+    if (spaces.length !== indentSpacesCount) {
+      return failure('indent')
+    }
+    const wordMatch = s.match(/\S+/y)
+    if (!wordMatch) {
+      return failure('expected command')
+    }
+    const newCommand = { name: wordMatch[0] }
+    pushParameter(currentCommand, newCommand)
+    currentCommand = newCommand
+    commandStack.push(newCommand)
+    indentSpacesCount += INDENT_SIZE
+    if (!s.match(/$/my)) {
+      return failure('expected end of line')
+    }
+    lineNumber++
+  }
+  return { type: 'success', result: completeAx(root) }
 }
 
 const root = document.getElementById('root')
